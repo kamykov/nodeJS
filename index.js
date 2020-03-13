9eead22afeb93776208572b2d807a17433684dc7
Krzysztof Kamieniecki <krzysztof.kamieniecki@crealogix.com>
2020-03-13 18:24:06 +0100

    Express first setup::

--- index.js
+++ index.js
@@ -1,48 +1,32 @@
 const fs = require('fs');
 const http = require('http');
 
+const express = require('express');
+
+
+
+
+const app = express();
+
+
+app.use((req, res, next) => {
+    console.log(" ==> z use ");
+    next();
+})
+app.use((req, res, next) => {
+    console.log(" ==> z use second ")
+    res.send('<h3>Response</h3>')
+})
+app.use((req, res, next) => {
+    console.log(" ==> z use ")
+})
+
+
+
+const routes = require('./routes')
+
 const PORT = 8080;
 
-function reqListener (req, res) {
-
-    const {url, method, } = req;
-
-    if(url === '/') {
-        res.setHeader('Content-Type', 'text/html');
-        res.write('<html>')
-        res.write('<head><title>NodeJS Sample</title></head>')
-        res.write('<body><h1>ELO FOLKS!</h1><form action="/message" method="POST"><input name="message" /><input type="submit" value="Submit"></form></body>');
-        res.write('</html>');
-        return res.end();
-    }
-
-    if (url === '/message' && method === "POST") {
-        const body = [];
-        req.on('data', (chunk) => {
-            console.log(chunk)
-            body.push(chunk);
-
-        })
-        return req.on('end', ()=> {
-            const parseBody = Buffer.concat(body).toString();
-            const message = parseBody.split('=')[1];
-            console.log(parseBody);
-            fs.writeFile('message.txt', message, (err) => {
-                res.statusCode = 302;
-                res.setHeader('Location', '/');
-                return res.end();
-            });
-        })
-    }
-
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html>')
-    res.write('<head><title>NodeJS Sample</title></head>')
-    res.write('<body><h1>Message send!</h1></body>')
-    res.write('</html>');
-    res.end();
-}
-
-const server = http.createServer(reqListener);
+const server = http.createServer(app);
 
 server.listen(PORT, () => console.log(`Listening on port ${PORT}`))
